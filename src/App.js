d098d10fc290fbdca6f2dad739864f71e46e9196
asahmed93 <56940751+asahmed93@users.noreply.github.com>
2020-01-13 15:17:27 -0500

    Added Initial State Values

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Pay Bills',
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: 'Save money',
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: 'Splurge',
+          id: Date.now(),
+          completed: false
+        }
+      ]
+    }
+  }
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos}/>
       </div>
     );
   }
